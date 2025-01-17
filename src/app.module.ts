32344c7b260bdcae61327d3d845de9099c4c87a8
Islam <islam.music@scv.si>
2025-01-17 12:33:27 +0100

    zacetek projekta

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MoviesModule } from './movies/movies.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Movie } from './movies/entities/movie';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'imdb',
+      entities: [Movie],
+      synchronize: true,
+    }),
+    MoviesModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
