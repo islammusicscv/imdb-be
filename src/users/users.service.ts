4b486210cae0f04834694847dbac873ec13672d2
Islam <islam.music@scv.si>
2025-01-31 11:52:09 +0100

    registracija

---
+++ src/users/users.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entity/user.entity';
+import { Repository } from 'typeorm';
+import { UserRegisterDto } from '../auth/user-register.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly userRepostory: Repository<User>,
+  ) {}
+
+  async create(user: UserRegisterDto): Promise<User> {
+    const newUser = this.userRepostory.create(user);
+    return await this.userRepostory.save(newUser);
+  }
+}
